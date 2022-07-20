5edfcd95a435653a8871fd30b4497945506030ac
Thomas Rasser <thomas.rasser.99@gmail.com>
2022-07-20 12:22:23 +0200

    Automated testing added

--- main.py
+++ main.py
@@ -7,13 +7,20 @@
 
     # Run the following command to run a challenge:
 
-    # python3 main.py -<challenge_number>
-        # e.g. python3 main.py -0
-    
-    # If there is already a challenge with <challenge_number> in the challenges folder, it will run the challenge, 
-    # otherwise it asks you, if a new challenge template should be created via a (y/n) question.
-    # "y" will create a new challenge with the name: "Challenge<challenge_number>.py"
-    # "n" will exit the program.
+        # python3 main.py -<challenge_number>
+            # e.g. python3 main.py -0
+        
+        # If there is already a challenge with <challenge_number> in the challenges folder, it will run the challenge, 
+        # otherwise it asks you, if a new challenge template should be created via a (y/n) question.
+        # "y" will create a new challenge with the name: "Challenge<challenge_number>.py"
+        # "n" will exit the program.
+
+    # Run the following command to run test(s):
+
+        # python3 main.py -t
+        # "-t" will run the test for all challenges
+
+    # -----------------------------------------------------
 
 #endregion
 
@@ -23,23 +30,54 @@
 import os
 import re
 import sys
+import subprocess
 
 # Parse input
-if(len(sys.argv) > 1 and re.match("^\-[0-9]{1,2}$", sys.argv[1]) != None):
-    active_challenge_filename = "Challenge" + sys.argv[1][1:].zfill(2) + ".py"
-    active_challenge_number = int(sys.argv[1][1:])
+if(len(sys.argv) == 2):
+    if(re.match("^\-[0-9]{1,2}$", sys.argv[1]) != None):
+        active_challenge_filename = "Challenge" + sys.argv[1][1:].zfill(2) + ".py"
+        active_challenge_number = int(sys.argv[1][1:])
+        challenge_test = False
+    elif(re.match("^\-t$", sys.argv[1]) != None):
+        challenge_test = True
+    else:
+        print("Error: Invalid argument")
+        exit()
 else :
     print("Error: invalid argument(s)")
     exit()
 
-# Run selected challenge if it exists
 print("Python Challenges: ")
 print("-----------------------------------------------------")
 
+# Run test for all challenges
+if(challenge_test):
+    # Get all challenge filenames
+    challenge_filenames = []
+    for filename in os.listdir("challenges"):
+        if(re.match("^Challenge[0-9]{1,2}\.py$", filename) != None):
+            challenge_filenames.append(filename)
+
+    # Run test for all challenges
+    if(len(challenge_filenames) > 0):
+        for challenge_filename in challenge_filenames:
+            return_code = subprocess.call(
+                "python3 challenges\\" + challenge_filename,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.STDOUT
+            )
+            if return_code == 0:
+                print(challenge_filename +   ": Passed")
+            else:
+                print(challenge_filename +   ": X Failed")
+    exit()
+
+# Run selected challenge if it exists
 if(os.path.isfile("challenges\\" + active_challenge_filename)):
     os.system('cls')
     os.system("python3 challenges\\" + active_challenge_filename)
-    exit()
+    exit()    
+# Ask if a new challenge should be created
 else:
     print("Error: challenge does not exist\n")
 
