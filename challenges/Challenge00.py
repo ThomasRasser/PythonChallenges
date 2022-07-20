8e6049f621bfa3f76f05f30dd62ec2892ff3fa51
Thomas Rasser <thomas.rasser.99@gmail.com>
2022-07-20 10:09:42 +0200

    Challenge 0 solved

---
+++ challenges/Challenge00.py
@@ -0,0 +1,16 @@
+#region Challenge 0
+import os
+
+print("Challenge 0: ")
+print("-----------------------------------------------------")
+
+# Caclculate the soltuion
+solution = str(2**38)
+print("2^38 = " + str(2**38))
+
+solution_url = "http://www.pythonchallenge.com/pc/def/" + str(2**38) + ".html"
+print("\n" + solution_url)
+os.system('echo ' + solution_url + ' | clip')
+
+print("-----------------------------------------------------\n\n")
+#endregion
