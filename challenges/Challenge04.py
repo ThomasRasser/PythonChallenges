e1a694720c734ac4979388402a633535ffb23e24
Thomas Rasser <thomas.rasser.99@gmail.com>
2022-07-20 10:14:32 +0200

    Challenge 4 solved

---
+++ challenges/Challenge04.py
@@ -0,0 +1,33 @@
+
+#region Challenge 4
+import os
+import urllib.request
+
+print("Challenge 4: ")
+print("-----------------------------------------------------")
+
+url_base = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="
+# cur_number = "12345"  # first index
+# cur_number = "8022"   # second index
+# cur_number = "63579"  # third index
+cur_number = "75635"    # fourth index
+
+for i in range(0, 400):
+    # Request url and decode to utf-8
+    url_request = urllib.request.urlopen(url_base + cur_number)
+    data = url_request.read()
+    data = data.decode("utf-8")
+    print(data)
+    
+    # Find number in data and update cur_number
+    cur_number = data.split(" ")[-1]
+    if(not cur_number.isdigit()):
+        solution = cur_number
+        break
+
+solution_url = "http://www.pythonchallenge.com/pc/def/" + solution
+print("\n" + solution_url)
+os.system('echo ' + solution_url + ' | clip')
+
+print("-----------------------------------------------------\n\n")
+#endregion
