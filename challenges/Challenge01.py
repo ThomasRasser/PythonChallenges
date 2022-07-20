9f16a5ac3e9a99883d904ac6937ab5e68d0b2a61
Thomas Rasser <thomas.rasser.99@gmail.com>
2022-07-20 11:51:57 +0200

    Fixed wrong challenge number

--- challenges/Challenge01.py
+++ challenges/Challenge01.py
@@ -1,7 +1,7 @@
-#region Challenge 2
+#region Challenge 1
 import os
 
-print("Challenge 2: ")
+print("Challenge 1: ")
 print("-----------------------------------------------------")
 
 code = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
